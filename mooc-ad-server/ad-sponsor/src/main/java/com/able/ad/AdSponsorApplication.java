27c5b60d9daa084b5c2d3b5a4391bb94e298f257
jipeng <jipeng@able-elec.com>
2019-09-05 19:59:55 +0800

    entry定义

--- mooc-ad-server/ad-sponsor/src/main/java/com/able/ad/AdSponsorApplication.java
+++ mooc-ad-server/ad-sponsor/src/main/java/com/able/ad/AdSponsorApplication.java
@@ -1,8 +1,12 @@
 package com.able.ad;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @param
@@ -11,9 +15,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@MapperScan(basePackages = "com.able.ad.mapper")
 public class AdSponsorApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdSponsorApplication.class, args);
+
     }
 }
 
