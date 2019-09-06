7967ee59a0524b8ef8298bdd7b50acc8ada0be04
jipeng <jipeng@able-elec.com>
2019-09-06 12:39:09 +0800

    å¢åmapper单元测试通过

--- mooc-ad-server/ad-sponsor/src/test/java/com/able/ad/mapper/AdPlanMapperTest.java
+++ mooc-ad-server/ad-sponsor/src/test/java/com/able/ad/mapper/AdPlanMapperTest.java
@@ -3,10 +3,13 @@ package com.able.ad.mapper;
 import com.able.ad.entry.AdPlan;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.List;
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class AdPlanMapperTest {
